b91861b78944d51586fb60c92679eda47a6733e5
MeganMFisher <meganmichelle318@gmail.com>
2017-07-07 23:38:57 -0600

    about us coming along!

--- app/src/components/about/contactForm/ContactForm.js
+++ app/src/components/about/contactForm/ContactForm.js
@@ -3,16 +3,21 @@ import './contactForm.css';
 
 export default function ContactForm() {
 
+    function handleChange(event) {
+        console.log(event.target.value)
+    }
+
 return (
         <div className='contactFormSection'>
                 <h1>Contact Us</h1>
             <div className='contactUsBox'>
-                <input placeholder='Firstname'/>
+                <input placeholder='Firstname' onChange={handleChange}/>
                 <input placeholder='Lastname'/>
                 <input placeholder='Email Address'/>
                 <input placeholder='Phone Number'/>
                 <input placeholder='Comment'/>
             </div>
+                <button>SUBMIT</button>
         </div>
 )
 
